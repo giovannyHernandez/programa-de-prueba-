60f87ea8e41f697d8ec2145734d86adffc2ea97a
ColsutecR <ColsutecR@ColsutecR-PC>
2016-02-08 19:59:10 -0500

    segunda version definicion de varibles y muestra de resultados

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,11 @@ package javaapplication1;
 public class JavaApplication1 {
 
     public static void main(String[] args) {
+        int a = 5+5;
+        int suma = a;
+        System.out.println("la suma es de : " + suma );
+        
+        
         
     }
     
